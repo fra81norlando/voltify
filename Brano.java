cdb146bef5cd1111eb26e5d9aa7feb9e4d591ad0
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-01-14 15:54:01 +0100

    Create Brano.java

---
+++ Brano.java
@@ -0,0 +1,16 @@
+package com.example.voltify;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Brano 
+{
+  private String titolo;
+  private int durata;
+  private String autore;
+  private Date datacreazione;
+  
+  public Brano(String titolo){
+    this.titolo = titolo;
+    }
+}
