a7a4d8e429fac97e1f412b4e8ad2576fb471c316
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-01-18 16:57:47 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -19,13 +19,13 @@ String[] elencogeneri = {"rock", "liscio", "pop", "rap", "R&B"};
   {
     super.onCreate(savedInstanceState);
     setContentView(R.Layout.activity_main);
-    btnInserisci=findViewById(R.id.btnInserisci);
-    txtitolo=findViewById(R.id.plain);
+    btnInserisci = findViewById(R.id.btnInserisci);
+    txtitolo = findViewById(R.id.plain);
     spnGenere = (Spinner)findViewById(R.id.spnGenere);
     ArrayAdapter<String> spGen = new ArrayAdapter<String>(context this, R.layout.support_simple_spinner_dropdown_item, elencogeneri);
     spnGenere.setAdapter(spGen);
     
-    gb= new GestoreBrani();
+    gb = new GestoreBrani();
     btnInserisci.setOnClickListener(newView.OnClickListener()
     {
       @Override
