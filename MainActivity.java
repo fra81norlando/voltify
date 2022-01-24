8fb59648db7cc51c13b737bdfc7e2bb3a233e425
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-01-24 17:12:49 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,74 @@
 package com.example.voltify;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity
-{
-Button btnInserisci;
-EditText txtitolo;
-GestoreBrani gb;
-Spinner spnGenere;
-String[] elencogeneri = {"rock", "liscio", "pop", "rap", "R&B"};
-  @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.Layout.activity_main);
-    btnInserisci = findViewById(R.id.btnInserisci);
-    txtitolo = findViewById(R.id.plain);
-    spnGenere = (Spinner)findViewById(R.id.spnGenere);
-    ArrayAdapter<String> spGen = new ArrayAdapter<String>(context this, R.layout.support_simple_spinner_dropdown_item, elencogeneri);
-    spnGenere.setAdapter(spGen);
-    
-    gb = new GestoreBrani();
-    btnInserisci.setOnClickListener(newView.OnClickListener()
+    Button btnInserisci;
+    Button btnVisualizza;
+    EditText txtitolo;
+    GestoreBrani gb;
+    Spinner spnGenere;
+    String[] elencoGeneri = {"Rock", "Rap", "Pop", "R&B"};
+    Button btnInserisci; 
+    Button btnVisualizza; 
+    EditText txtitolo; 
+    GestoreBrani gb; 
+    Spinner spnGenere; 
+    String[] elencoGeneri = {"Rock", "Rap", "Pop", "R&B"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
     {
-      @Override
-      public void onClick(View view)
-      {
-        String testo = spnGenere.getSelectedItem().toString();
-        gb.addBrano(txtitolo.getText().toString());
-      }
-    }];
-   }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnInserisci=findViewById(R.id.btnInserisci);
+        btnVisualizza=findViewById(R.id.btnVisualizza);
+        txtitolo=findViewById(R.id.titolo);
+        spnGenere=(Spinner) findViewById(R.id.spnGenere);
+        btnInserisci=findViewById(R.id.btnInserisci); 
+        btnVisualizza=findViewById(R.id.btnVisualizza); 
+        txtitolo=findViewById(R.id.titolo); 
+        spnGenere=(Spinner) findViewById(R.id.spnGenere);
+        ArrayAdapter<String> spGen = new ArrayAdapter<String>(this, R.layout.activity_main, elencoGeneri);
+        spnGenere.setAdapter(spGen);
+        
+        spnGenere.setAdapter(spGen);
+        gb= new GestoreBrani();
+        gb= new GestoreBrani();
+        btnInserisci.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v)
+                    {
+                        gb.addBrano(txtitolo.getText().toString());
+                        gb.addBrano(txtitolo.getText().toString(),spnGenere.getSelectedItem().toString());
+                       
+                        Log.d("funzionamento MainActivity", "funziona metodo btnInserisci.setOnClickListener");
+                        
+                    }
+                }
+        );
+
+        btnVisualizza.setOnClickListener(
+                new Button.OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+                        gb.visualizza();
+                        Intent i = new Intent(MainActivity.this, GestoreBrani.class);
+                        startActivity(i);
+                        Intent i = new Intent(MainActivity.this, GestoreBrani.class);
+                        startActivity(i); 
+                        Log.d("funzionamento", "funziona metodo btnVisualizza.setOnClickListener");
+                        
+                    }
+                }
+        );
+    }
 }
